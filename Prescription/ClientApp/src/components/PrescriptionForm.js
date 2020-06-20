1283ab23619519777f4b411e78581ca9ffb49560
saipurnimag <saipurnima@outlook.com>
2020-06-20 21:07:34 +0530

    Speech to text basic

--- Prescription/ClientApp/src/components/PrescriptionForm.js
+++ Prescription/ClientApp/src/components/PrescriptionForm.js
@@ -1,21 +1,66 @@
-﻿import React from 'react';
+﻿import React, { useState } from 'react';
 import { Form, Input, InputNumber, Button } from 'antd';
-
+import { AudioOutlined } from "@ant-design/icons";
 
 
 const PrescriptionForm = () => {
 
-      const validateMessages = {
-      required: '${label} is required!',
-      types: {
-            email: '${label} is not validate email!',
-            number: '${label} is not a validate number!',
-      },
-      number: {
-            range: '${label} must be between ${min} and ${max}',
-      },
-};
+      const [isListening, listen] = useState(false);
+      var SpeechRecognition = SpeechRecognition || window.webkitSpeechRecognition
+      var SpeechGrammarList = SpeechGrammarList
+      var SpeechRecognitionEvent = SpeechRecognitionEvent
+      const recognition = new SpeechRecognition()
 
+      recognition.continous = true
+      recognition.interimResults = true
+      recognition.lang = "en-IN"
+
+      const validateMessages = {
+            required: '${label} is required!',
+            types: {
+                  email: '${label} is not validate email!',
+                  number: '${label} is not a validate number!',
+            },
+            number: {
+                  range: '${label} must be between ${min} and ${max}',
+            },
+      };
+
+
+
+      const [name, setName] = useState([""]);
+      const[status,setStatus] = useState(["Start"]);
+
+      const record = () => {
+
+
+            listen(!isListening);
+            if(status=="Start") setStatus("Stop")
+            else setStatus("Start")
+            if (isListening) {
+                  //handle listen
+                  recognition.start();
+                  var finalTranscript = "";
+                  recognition.onresult = (event) => {
+                        var interimTranscript = "";
+
+                        for (var i = event.resultIndex; i < event.results.length; ++i) {
+                              const transcript = event.results[i][0].transcript;
+                              if (event.results[i].isFinal) finalTranscript += transcript + ' ';
+                              else interimTranscript += transcript;
+                        }
+                        console.log("interimTranscript = " + interimTranscript);
+                        console.log("name = " + { name });
+                        console.log("finalTranscript = " + finalTranscript);
+                        setName(interimTranscript);
+                        setName(finalTranscript);
+
+                  }
+
+
+            }
+
+      }
 
       const onFinish = values => {
             console.log(values);
@@ -27,15 +72,25 @@ const PrescriptionForm = () => {
       };
 
       return (
-            <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
+            <Form name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
+                  Name is {name}
                   <Form.Item name={['user', 'name']} label="Name" rules={[{ required: true }]}>
-                        <Input />
+                        <Input.TextArea
+                              defaultValue={name}
+                              value={name}
+                              placeholder={name}
+                              enterButton="Search"
+                              size="large"
+                        />
+                        <Button type="primary" onClick={record}>
+                              {status}
+                        </Button>
                   </Form.Item>
                   <Form.Item name={['user', 'email']} label="Email" rules={[{ type: 'email' }]}>
                         <Input />
                   </Form.Item>
                   <Form.Item name={['user', 'age']} label="Age" rules={[{ type: 'number', min: 0, max: 99 }]}>
-                        <InputNumber />
+                        <Input />
                   </Form.Item>
                   <Form.Item name={['user', 'website']} label="Website">
                         <Input />
