907454177ebe50722bdc5f2c4cb8113163b194ef
Purnima <saipurnima@outlook.com>
2020-06-25 01:09:05 +0530

    new react app

---
+++ voiceprescription/src/Components/RightMenu.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { Menu, Icon } from "antd";
+import { MailOutlined, AppstoreOutlined, SettingOutlined, LoginOutlined, LockOutlined, GithubOutlined, HomeOutlined } from '@ant-design/icons'
+
+const { SubMenu } = Menu;
+
+const RightMenu = () =>{
+  const [current, setCurrent] = useState("mail");
+  const handleClick = (event) =>{
+    setCurrent(event.key)
+  }
+  return (
+    <Menu
+      onClick={handleClick}
+      selectedKeys={current}
+      mode="horizontal"
+    >
+      <SubMenu key="Home" icon={<HomeOutlined />} title="Home">
+        Home
+      </SubMenu>
+      <SubMenu key="Login" icon={<SettingOutlined />} title="Login">
+        Login
+      </SubMenu>
+      <SubMenu key="Prescription" icon={<LoginOutlined />} title="Prescription">
+        Prescription
+      </SubMenu>
+      <SubMenu key="Register" icon={<LockOutlined />} title="Register">
+        Login
+      </SubMenu>
+      <SubMenu key="About us" icon={<AppstoreOutlined />} title="About us">
+        About us
+      </SubMenu>
+      <SubMenu key="Github" icon={<GithubOutlined />} title="Github">
+        About us
+      </SubMenu>
+      
+    </Menu>
+  );
+}
+ 
+export default RightMenu;
